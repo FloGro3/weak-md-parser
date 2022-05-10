1a6e6c21b706cca8131b139225207c4f8e084846
Florian Tobias Grotke <fg877820@fh-muenster.de>
2022-05-10 21:55:33 +0200

    JavaDoc Kommentare hinzugefügt

--- src/main/java/com/github/arena/challenges/weakmdparser/MarkdownParser.java
+++ src/main/java/com/github/arena/challenges/weakmdparser/MarkdownParser.java
@@ -3,16 +3,25 @@ package com.github.arena.challenges.weakmdparser;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Klasse die einen String mit einer Markdown-Syntax in einen HTML-String übersetzt
+ */
 public class MarkdownParser {
 
     public MarkdownParser() {
     }
 
+    /**
+     * Hauptmethode der Klasse. Wandelt den übergebenen String in einen HTML-String um
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     public String parse(String markdown) {
         String[] lines = markdown.split("\n");
         String result = "";
-        List<String> listItems = new ArrayList<String>();
+        List<String> listItems = new ArrayList<>();
 
+        //StringBuilder oder StringBuffer einsetzen für bessere Performance bei großen Eingabemengen
         for (int i = 0; i < lines.length; i++) {
 
             if (isListItem(lines[i])){
@@ -39,6 +48,11 @@ public class MarkdownParser {
         return result;
     }
 
+    /**
+     * Wandelt den übergebenen String in ein HTML-Heading um. Die # am Anfang geben dabei die Art des Headings an. # -> h1; ## -> h2 ...
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     private String parseHeader(String markdown) {
         int count = 0;
 
@@ -53,19 +67,39 @@ public class MarkdownParser {
         }
     }
 
+    /**
+     * Wandelt den übergebenen String in ein HTML-ListItem um. Der String muss mit * beginnen.
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     private String parseListItem(String markdown) {
         return "<li>" + markdown.substring(2) + "</li>";
     }
 
+    /**
+     * Wandelt den übergebenen String in einen HTML-Paragraph um.
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     private String parseParagraph(String markdown) {
         return "<p>" + markdown + "</p>";
     }
 
+    /**
+     * Bettet die Wörter entsprechend der Notation im String in HTML-bold oder HTML-italic ein. _Wort_ -> Wort wird in italic eingebettet; __Wort__ -> Wort wird in bold eingebettet.
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     private String parseInnerContent(String markdown) {
         String boldParsed = parseBold(markdown);
         return parseItalic(boldParsed);
     }
 
+    /**
+     * Bettet die Wörter, die von einem Unterstrich umgeben sind, in HTML-italic ein.
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     private String parseItalic(String markdown){
         //Um die Methode vor parseBold() verwenden zu können, müsste das Erkennen des TeilStrings verändert werden, z.B. per RegExp
         String lookingFor = "_(.+)_";
@@ -73,14 +107,24 @@ public class MarkdownParser {
         return markdown.replaceAll(lookingFor, update);
     }
 
+    /**
+     * Bettet die Wörter, die von zwei Unterstrichen umgeben sind, in HTML-italic ein.
+     * @param markdown String mit der Markdown-Syntax, der übersetzt werden soll
+     * @return Übersetzter HTML-String
+     */
     private String parseBold(String markdown){
         String lookingFor = "__(.+)__";
         String update = "<strong>$1</strong>";
         return markdown.replaceAll(lookingFor, update);
     }
 
+    /**
+     * Bettet die übergebenen Listitems in eine HTML-unorderdList ein.
+     * @param listItems Liste mit den Items die eingebettet werden sollen
+     * @return Übersetzter HTML-String
+     */
     private String parseList(List<String> listItems){
-        //Für viele aufeinander folgende ListItems besser einen StringBuilder oder StringBuffer für bessere Perfomance einsetzen
+        //Für viele aufeinander folgende ListItems besser einen StringBuilder oder StringBuffer für bessere Performance einsetzen
         String result = "";
         for (String listItem:listItems){
             result += parseListItem(listItem);
@@ -88,19 +132,21 @@ public class MarkdownParser {
         return "<ul>" + result + "</ul>";
     }
 
+    /**
+     * Prüft, ob der übergebene String die Markdown-Syntax für ein HTML-ListItem erfüllt.
+     * @param markdown zu prüfender String
+     * @return True, wenn der String die Markdown-Syntax für ein HTML-Listitem erfüllt, ansonsten false
+     */
     private boolean isListItem(String markdown){
-        if (markdown.startsWith("*")){
-            return true;
-        } else {
-            return false;
-        }
+        return markdown.startsWith("*");
     }
 
+    /**
+     * Prüft, ob der übergebene String die Markdown-Syntax für ein HTML-Heading erfüllt.
+     * @param markdown zu prüfender String
+     * @return True, wenn der String die Markdown-Syntax für ein HTML-Heading erfüllt, ansonsten false
+     */
     private boolean isHeader(String markdown){
-        if (markdown.startsWith("#")){
-            return true;
-        } else {
-            return false;
-        }
+        return markdown.startsWith("#");
     }
 }
